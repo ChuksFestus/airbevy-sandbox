977c374fc57ed6585b22654d23b2a092f198ef63
Chuks Festus <cutogu@gmail.com>
2018-03-07 11:41:10 +0100

    updated port

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import bodyParser from "body-parser";
 import cors from "cors";
 
 const app = express();
-const port = 7000;
+const port = process.env.PORT || 7000;
+
+const company = "kariXchange";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -39,6 +41,38 @@ app.get("/api/v1/posts", (req, res) => {
     }
   ];
   res.json(posts);
+});
+
+app.get("/api/v1/profile", (req, res) => {
+  let profilePost = [
+    {
+      companyName: `kaie`,
+      companyPic: `https://karixchange.com/wp-content/uploads/2016/09/Logo_third.png`,
+      post: "KariXchange Signs Contracts for the NPFL Platform with LMC",
+      postPic: `https://karixchange.com/wp-content/uploads/2016/11/LMC-COOSalihu-Abubakar-Kari-Xchange-CEO-Eucharia-Amanambu-LMC-ChairmanShehu-Dikko-Rasheed-LMC-Legal-Adviser-Sampson-Ebomhe-1-1-800x578.jpg`,
+      NumLikes: 20,
+      numComments: 2
+    },
+    {
+      companyName: `kaie`,
+      companyPic: `https://karixchange.com/wp-content/uploads/2016/09/Logo_third.png`,
+      post:
+        "Digital products should be both useful and beautiful. Therefore, our process is focused on understanding user needs and your brand, so that we can meet and exceed people’s functional expectations of your product while delivering a product form that embodies your brand personality and delights users. ",
+      postPic: `http://static.adweek.com/adweek.com-prod/wp-content/uploads/sites/2/2015/05/FbStartLogo2015.jpg`,
+      NumLikes: 5,
+      NumComments: 6
+    },
+    {
+      companyName: `kaie`,
+      companyPic: `https://karixchange.com/wp-content/uploads/2016/09/Logo_third.png`,
+      post:
+        "KariXchange design team have a highly developed sensitivity to refined aesthetics. We work hard to ensure that our client’s product concepts combine the excitement of inspired design with the practicality of design for manufacture and cost sensitivity.",
+      postPic: ``,
+      NumLikes: 15,
+      NumComments: 6
+    }
+  ];
+  res.json(profilePost);
 });
 
 app.listen(port, () => {
